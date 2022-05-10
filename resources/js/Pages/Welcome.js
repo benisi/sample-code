da07d5f746108183e5a2e901a29140e7ce641bd9
Benjamin Isidahomen <bisidahomen@gmail.com>
2022-05-10 04:53:36 +0100

    chore: bootstrapping

---
+++ resources/js/Pages/Welcome.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link, Head } from "@inertiajs/inertia-react";
+
+export default function Welcome(props) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="">
+                <div className="">
+                    {props.auth.user ? (
+                        <Link
+                            href={route("dashboard")}
+                            className="text-sm text-gray-700 underline"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route("login")}
+                                className="text-sm text-gray-700 underline"
+                            >
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route("register")}
+                                className="ml-4 text-sm text-gray-700 underline"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+
+                <div className="">
+                    <h3>Posts</h3>
+                </div>
+            </div>
+        </>
+    );
+}
